6da18e4045dccef756e72540860ee8b9248341e2
Tran Van Long <tranvlong04081999@gmail.com>
2024-04-28 00:00:45 +0700

    add loading

---
+++ LongLinhWedding/Controllers/LoadingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LongLinhWedding.Controllers
+{
+    public class LoadingController : Controller
+    {
+        // GET: LoadingController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
